6738a6c10d9c3f677fba7c7e6de5d3fdc857e486
Fagner <fagner.egomes@gmail.com>
2021-01-13 16:43:33 -0300

    feat: 🎸 Criamos o login e signout no firebase
    Não esquecer de configurar as variáveis de ambiente no envirenment

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides, IonSegment } from '@ionic/angular';
+import { IonSlides, IonSegment, LoadingController, ToastController } from '@ionic/angular';
+
+import { User } from '../../interfaces/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +15,11 @@ export class LoginPage implements OnInit {
 
   public wavesPosition = 0;
   public wavesDifference = 100;
+  public userLogin: User = {};
+  public userRegister: User = {};
+  private loading: any;
 
-  constructor() {}
+  constructor(private loadingCtrl: LoadingController, private toastCtrl: ToastController, private authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -36,5 +42,39 @@ export class LoginPage implements OnInit {
       this.segment.value = 'cadastro';
       this.wavesPosition -= this.wavesDifference;
     }
+  }
+
+  async login() {
+    await this.presentLoading();
+
+    try {
+      await this.authService.login(this.userLogin);
+    } catch (error) {
+      await this.presentToast(error.message);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async register() {
+    await this.presentLoading();
+
+    try {
+      await this.authService.register(this.userRegister);
+    } catch (error) {
+      await this.presentToast(error.message);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({ message: 'Por favor, aguarde...' });
+    return this.loading.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 4000 });
+    toast.present();
   }
 }
